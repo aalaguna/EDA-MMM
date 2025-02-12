84c07a56e2069c3c9ab0ab0079107d9e52d5ae3e
aalaguna <aura.alaguna@groupm.com>
2025-02-12 15:23:06 -0500

    Deleted files that where duplicated and some of them where not needed

--- EDA/Scripts/S-Curve Info Table.R
+++
@@ -1,9 +0,0 @@
-# EDA/Scripts/S-Curve Info Table.R
-
-s_curve_chart_info_df <- tibble::tribble(
-  ~Metric,      ~Value,
-  "Alpha",      alpha,
-  "Beta",       beta,
-  "MaxVal%",    max_val_pct,
-  "MaxVal",     max_activity[1] * (max_val_pct / 100)
-)
